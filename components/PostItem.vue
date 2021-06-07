c0be830433373940cb3326184ecff2b302c49b07
Pvvdee <godpvvdee@gmail.com>
2021-06-07 12:23:35 +0800

    nuxt beginner

---
+++ components/PostItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="post">
+    <a class="post-header post-header-link clickable">
+      <h4 class="title is-4"> {{title}} <br> Гарчиг</h4>
+      <h5 class="subtitle is-5"> {{subtitle}}  <br> Дэд гарчиг</h5>
+    </a>
+    <div class="post-content">
+      by Pvrew-Erdene, 4th , Jun, 2021
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      require: true
+    },
+    subtitle: {
+      type: String,
+      require: false
+    }
+  }
+}
+</script>
+
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css"
+    />
+    <navbar />
+    {{title}}
+    <div class="blogs-page">
+      <div class="main-content">
+        <div class="container">
+          <div class="columns is-mobile">
+            <div class="column is-8 is-offset-2">
+              <div class="section">
+                <div class="title">
+                  <h1>Шинэ мэдээ</h1>
+                  <hr />
+                </div>
+                <post-item
+                  v-bind:title="title"
+                  v-bind:subtitle="'New Post Subtitle'"
+                />
+                <post-item
+                  v-bind:title="'New Post Title2'"
+                  v-bind:subtitle="'New Post Subtitle2'"
+                />
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import Postitem from "~/components/PostItem.vue";
+// import Postitem from "~/components/PostItem.vue";
+export default {
+
+  data () {
+    return {
+      title: 'this is fucking title'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.post-content {
+  font-style: italic;
+}
+.post {
+  margin-bottom: 20px;
+  padding: 5px;
+  border-bottom: 2px solid transparent;
+}
+.post:hover {
+  border-bottom: 2px solid #e8e8e8;
+}
+</style>
